9c4f72a7ba92d0bbd619d98d12d12bfdce1d8179
Юрий Мисюра <apriory13@meta.ua>
2023-12-05 15:17:08 +0200

    make build

---
+++ index.93b4f855.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".spider");e.style.top="50%",e.style.left="50%",e.style.transform="translate(-50%, -50%)"});
+//# sourceMappingURL=index.93b4f855.js.map
